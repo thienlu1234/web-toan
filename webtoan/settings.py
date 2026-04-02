ea2b84248004ce4b585a912c1b963e7ca23caeae
thienlu1234 <vanlu2392002@gmail.com>
2026-04-03 00:48:25 +0700

    fix middleware order

--- webtoan/settings.py
+++ webtoan/settings.py
@@ -41,14 +41,17 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # 👈 PHẢI ở đây
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
 ]
 
 ROOT_URLCONF = 'webtoan.urls'
@@ -125,3 +128,4 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
