4de3638ae95add4aa4b5dbbd2a1c0f83f37b64c2
thienlu1234 <vanlu2392002@gmail.com>
2026-04-03 17:31:27 +0700

    Hoàn thành xem video bài học

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+import re
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -22,7 +22,26 @@ class Course(models.Model):
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    video = models.FileField(upload_to='videos/')
+    video_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
-        return self.name    
+        return self.name
+
+    def get_embed_url(self):
+        if not self.video_url:
+            return ""
+
+        url = self.video_url.strip()
+
+        if "youtube.com/watch?v=" in url:
+            video_id = url.split("watch?v=")[-1].split("&")[0]
+            return f"https://www.youtube.com/embed/{video_id}"
+
+        if "youtu.be/" in url:
+            video_id = url.split("youtu.be/")[-1].split("?")[0]
+            return f"https://www.youtube.com/embed/{video_id}"
+
+        if "youtube.com/embed/" in url:
+            return url
+
+        return ""
